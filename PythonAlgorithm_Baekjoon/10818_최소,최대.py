671414cc42c0533c3bb460db32e29ac8203a1f99
KimJoongHyeok <moon960427@gmail.com>
2021-09-17 20:08:07 +0900

    백준알고리즘_최대,최소값구하기

---
+++ PythonAlgorithm_Baekjoon/10818_최소,최대.py
@@ -0,0 +1,8 @@
+N = int(input())
+A = list(map(int,input().split()))
+print(min(A),max(A))
+
+import sys
+N = sys.stdin.readline()
+A = list(map(int,sys.stdin.readline().split()))
+print(min(A),max(A))
